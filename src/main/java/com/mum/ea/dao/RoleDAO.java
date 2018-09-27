f1eaeccedf639539ee9a4730c577847408a5847c
Ryan <ryan.mousa14@gmail.com>
2018-09-27 00:37:47 -0500

    Fix Dao

--- src/main/java/com/mum/ea/dao/RoleDAO.java
+++ src/main/java/com/mum/ea/dao/RoleDAO.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import com.mum.ea.entities.Role;
 
 public interface RoleDAO extends JpaRepository<Role, Integer> {
-	@Query("select r.roleName from Role r, User_Role ur where ur.role_id = r.id and ur.id = :id")
+//	@Query("select r.roleName from Role r, User_Role ur where ur.role_id = r.id and ur.id = :id")
+	@Query("select r.roleName from Role r")
 	public List<String> findRolesName(@Param("id") Long id);
 }
