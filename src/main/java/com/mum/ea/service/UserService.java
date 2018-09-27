401f1d85de1075caf41ced57ef68c96c11999ff0
Moustafa <moustafa.sayed66@gmail.com>
2018-09-26 23:20:17 -0500

    user sevice update

---
+++ src/main/java/com/mum/ea/service/UserService.java
@@ -0,0 +1,20 @@
+package com.mum.ea.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mum.ea.entities.User;
+import com.mum.ea.dao.UserDAO;
+
+@Service
+@Transactional
+public class UserService {
+
+	@Autowired
+	private UserDAO userDAO;
+	public void save(User user) {
+		userDAO.save(user);
+		
+	}
+}
