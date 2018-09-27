94bebfc913cedefbac56958d495b9bd93f01d778
Moustafa <moustafa.sayed66@gmail.com>
2018-09-26 23:17:35 -0500

    update sign up

--- src/main/java/com/mum/ea/blog/BlogApplication.java
+++ src/main/java/com/mum/ea/blog/BlogApplication.java
@@ -2,11 +2,15 @@ package com.mum.ea.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.mum.ea")
+@EntityScan(basePackages="com.mum.ea")
+@EnableJpaRepositories(basePackages="com.mum.ea.dao")
 public class BlogApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mum/ea/dao/UserDAO.java
+++ src/main/java/com/mum/ea/dao/UserDAO.java
@@ -1,8 +1,13 @@
 package com.mum.ea.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.mum.ea.entities.User;
 
+@Repository
 public interface UserDAO extends JpaRepository<User, Integer> {
 
+	
+		
 }
